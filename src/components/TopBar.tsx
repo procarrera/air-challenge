f75f5085f15714765c2ef22b2e8e78f16d79b3d2
Pedro Carrera <procarrera@gmail.com>
2023-12-14 23:34:13 -0300

    Refactor TopBar component layout and styles

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -3,16 +3,24 @@ import { Search } from 'lucide-react'
 export default function TopBar() {
   return (
     <div className="flex flex-row flex-wrap md:flex-nowrap items-center justify-between gap-4 w-full mb-16 mt-8">
-      <div className="flex flex-row gap-2 w-full border rounded-full p-2" style={{ maxWidth: '400px' }}>
+      <div
+        className="flex flex-row gap-2 w-full border rounded-full p-2"
+        style={{ maxWidth: '400px' }}
+      >
         <Search size={24} />
         <input
-          type='text'
+          type="text"
           className="w-full bg-transparent outline-none"
-          placeholder="Search" />
+          placeholder="Search"
+        />
       </div>
       <div className="flex flex-row gap-2 min-w-fit">
-        <button className="bg-slate-950 hover:bg-slate-600 text-white font-bold py-2 px-4 rounded-md">Save changes</button>
-        <button className="text-gray-500 hover:text-gray-600 border font-bold py-2 px-4 rounded-md">Cancel</button>
+        <button className="bg-slate-950 hover:bg-slate-600 text-white font-bold py-2 px-4 rounded-md">
+          Save changes
+        </button>
+        <button className="text-gray-500 hover:text-gray-600 border font-bold py-2 px-4 rounded-md">
+          Cancel
+        </button>
       </div>
     </div>
   )
