dc6014e9eaeaf9eb94f08a6cfbd2be37319ab5cb
Pedro Carrera <procarrera@gmail.com>
2023-12-19 21:16:55 -0300

    Add loading spinner for image loading in BoardItemCard component

--- src/components/BoardItemCard.tsx
+++ src/components/BoardItemCard.tsx
@@ -1,19 +1,39 @@
 import { SubBoardItem } from '@/types/SubBoardItem'
+import { useState } from 'react';
+import { Shell } from 'lucide-react';
 
 interface BoardItemCardProps {
   subBoard: SubBoardItem
 }
 
+const LoadingImage = () => {
+  return (
+    <div className="w-full h-full flex items-center justify-center">
+      <Shell
+        className="w-8 h-8 text-gray-200 animate-spin"
+        size={32}
+        strokeWidth={2} />
+    </div>
+  );
+};
+
 export function BoardItemCard({ subBoard }: BoardItemCardProps) {
+  const [isImageLoaded, setImageLoaded] = useState(false);
+
   return (
     <li className="lg:max-w-xl w-full md:w-60 h-60 rounded overflow-hidden shadow-lg relative">
       <div className="h-full">
         {subBoard.thumbnails && (
-          <img
-            className="w-full h-full object-cover"
-            src={subBoard.thumbnails[0]}
-            alt="Sunset in the mountains"
-          />
+          <>
+            {!isImageLoaded && <LoadingImage />}
+            <img
+              className="w-full h-full object-cover"
+              src={subBoard.thumbnails[0]}
+              alt="Sunset in the mountains"
+              onLoad={() => setImageLoaded(true)}
+              style={isImageLoaded ? {} : { display: 'none' }}
+            />
+          </>
         )}
         {!subBoard.thumbnails && (
           <div className="w-full h-full bg-gray-300"></div>
@@ -23,5 +43,5 @@ export function BoardItemCard({ subBoard }: BoardItemCardProps) {
         {subBoard.title}
       </h3>
     </li>
-  )
+  );
 }
