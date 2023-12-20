3d3620e5a30500f54e57db8e4c30055d50f6399e
Pedro Carrera <procarrera@gmail.com>
2023-12-20 00:43:08 -0300

    Refactor code for board page component

--- src/app/boards/[board_id]/page.tsx
+++ src/app/boards/[board_id]/page.tsx
@@ -5,7 +5,11 @@ import ClipsList from '@/components/Clips/ClipsList'
 import { DndProvider } from 'react-dnd'
 import { HTML5Backend } from 'react-dnd-html5-backend'
 
-import { Box, boxesIntersect, useSelectionContainer } from '@air/react-drag-to-select'
+import {
+  Box,
+  boxesIntersect,
+  useSelectionContainer,
+} from '@air/react-drag-to-select'
 import { useEffect, useRef, useState } from 'react'
 
 interface BoardPageProps {
@@ -17,10 +21,10 @@ interface BoardPageProps {
 export default async function BoardPage({ params }: BoardPageProps) {
   const name = 'Air Branded Boards'
   const description = 'With a bunch of stock photos!'
-  const [selectionBox, setSelectionBox] = useState<Box>();
-  const [selectedIndexes, setSelectedIndexes] = useState<number[]>([]);
-  const selectableItems = useRef<Box[]>([]);
-  const elementsContainerRef = useRef<HTMLDivElement | null>(null);
+  const [selectionBox, setSelectionBox] = useState<Box>()
+  const [selectedIndexes, setSelectedIndexes] = useState<number[]>([])
+  const selectableItems = useRef<Box[]>([])
+  const elementsContainerRef = useRef<HTMLDivElement | null>(null)
   const { DragSelection } = useSelectionContainer({
     eventsElement: elementsContainerRef.current,
     selectionProps: {
@@ -31,31 +35,31 @@ export default async function BoardPage({ params }: BoardPageProps) {
       },
     },
     onSelectionChange: (box) => {
-      console.log("OnSelectionChange");
+      console.log('OnSelectionChange')
     },
     onSelectionStart: () => {
-      console.log("OnSelectionStart");
+      console.log('OnSelectionStart')
     },
-    onSelectionEnd: () => console.log("OnSelectionEnd"),
+    onSelectionEnd: () => console.log('OnSelectionEnd'),
     isEnabled: true,
-  });
+  })
 
   useEffect(() => {
     if (elementsContainerRef.current) {
       Array.from(elementsContainerRef.current.children).forEach((item) => {
-        const { left, top, width, height } = item.getBoundingClientRect();
+        const { left, top, width, height } = item.getBoundingClientRect()
         selectableItems.current.push({
           left,
           top,
           width,
           height,
-        });
-      });
+        })
+      })
     }
-  }, []);
+  }, [])
 
   return (
-    <div className='relative' ref={elementsContainerRef}>
+    <div className="relative" ref={elementsContainerRef}>
       <DragSelection />
       <div className="flex flex-col items-start justify-between gap-2 pb-8 border-b-2 w-full">
         <h2 className="text-4xl font-bold">{name}</h2>
